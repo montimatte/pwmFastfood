98839b7b9931085635bb54677c83315b231872e3
PC\master <matte.monti.06@gmail.com>
2026-07-13 18:41:22 +0200

    eliminazione della pagina di login dedicata che è diventata la pagina di index; sistemazione della grafica delle pagine e commenti; creazione di un css personalizzato per la grafice della pagina di login; login.html: aggiunto pulsante che porta alla pagina di registrazione, sistemazione dei campi in un form e modifica funzione di handling dati; signup.html:  aggiunto pulsante che porta alla pagina di login, sistemazione dei campi in un form e modifica funzione di handling dati; cliente.html & ristoratore.html: aggiunta della navbar e delle funzioni per il controllo dell'autenticazione e logout; account.html: creazione della pagina per visualizzare, modificare o eliminare l'account con gestione specifica dei tipi di account; main.js: sistemazione della nomenclatura, sistemazione dell'invio dei risultati in modo che tutti inviino un JSON, aggiunta dei metodi per ottenere i dati di un account, modificarli e cancellarli.

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ app.post('/login', async (req, res) => {
     const password = req.body.password;
     const client = await MongoClient.connect(mongoURL);
     const coll = client.db('Fastfood').collection('user');
-    const filter = {username:username, password: password}
+    const filter = {username:username, password: password} //cerca utente con username e password date
   
     const cursor = coll.find(filter);
     const result = await cursor.toArray();
@@ -30,7 +30,7 @@ app.post('/login', async (req, res) => {
     }
 });
 
-app.post('/signup', async (req, res) => {
+app.post('/account', async (req, res) => {
     const nome = req.body.nome;
     const cognome = req.body.cognome;
     const email = req.body.email;
@@ -45,26 +45,24 @@ app.post('/signup', async (req, res) => {
         datacarta = req.body.datacarta;
         cvvcarta = req.body.cvvcarta;
     }
-    //Inizio logica di controllo dei dati
 
     if (nome.length < 2) {
-        res.status(401).send("Nome troppo corto");
+        res.status(401).json({success: false, message: "Nome troppo corto"});
     }
     if (cognome.length < 2) {
-        res.status(401).send("Cognome troppo corto");
+        res.status(401).json({success: false, message: "Cognome troppo corto"});
     }
     if (email.length < 6) {
-        res.status(401).send("email non valida");
+        res.status(401).json({success: false, message: "email non valida"});
     }
     if (username.length < 2) {
-        res.status(401).send("Username troppo corto");
+        res.status(401).json({success: false, message: "Username troppo corto"});
     }
     if (password.length < 2) {
-        res.status(401).send("Password troppo corta");
+        res.status(401).json({success: false, message: "Password troppo corto"});
     }
 
     try {
-        //Fine logica di controllo dei dati
         const client = await MongoClient.connect(mongoURL);
         const coll = client.db('Fastfood').collection('user');
         const user = {
@@ -90,14 +88,101 @@ app.post('/signup', async (req, res) => {
       
     } catch (error) {
         console.log(error);
-        if (error.code == 11000) {
-            res.status(409).json({ success: false, message: "Username o email già in uso" });
+        if (error.code == 11000) { //conflitto sugli indici DB
+            res.status(409).json({ success: false, message: "Username o email già in uso" }); //errore di conflitto HTML
         } else {
             res.status(500).json({ success: false, message: "Errore non gestito" });
         }
     }
     res.send();
 });
+
+app.get('/user/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+        const client = await MongoClient.connect(mongoURL);
+        const coll = client.db('Fastfood').collection('user');
+        const cursor = coll.find(new ObjectID(id));
+        const result = await cursor.toArray();
+        await client.close();
+        if(result.length==0){
+            res.status(404).json({success: false, message: "Utente non trovato"});
+        }
+        else{
+            console.log("Utente trovato:");
+            console.log(result[0]);
+            res.status(200).json({success: true, message: "Utente Trovato", user: JSON.stringify(result[0])});
+        }
+    } catch (error) {
+        res.status(500).json({ success: false, message: "Errore non gestito" });
+    }
+})
+
+
+app.put('/user/:id', async (req, res) => {
+    const id = req.params.id;
+    const nome = req.body.nome;
+    const cognome = req.body.cognome;
+    const email = req.body.email;
+    const password = req.body.password;
+    const username = req.body.username;
+    const tipo = req.body.tipo;
+    let ncarta;
+    let datacarta;
+    let cvvcarta;
+    if(tipo=="cliente"){
+        ncarta = req.body.ncarta;
+        datacarta = req.body.datacarta;
+        cvvcarta = req.body.cvvcarta;
+    }
+    try {
+        const client = await MongoClient.connect(mongoURL);
+        const coll = client.db('Fastfood').collection('user');
+
+        const user = {
+            nome: nome,
+            cognome: cognome,
+            username: username,
+            email: email,
+            password: password,
+            tipo: tipo
+        };
+
+        if (tipo=="cliente") {
+            user.ncarta = ncarta;
+            user.datacarta = datacarta;
+            user.cvvcarta = cvvcarta;
+        }
+
+        const cursor = await coll.replaceOne({_id: new ObjectID(id)}, user); //sovrascrive tutti i dati (no ID) con quelli nuovi
+        //const result = await cursor.toArray();
+        await client.close();
+
+        console.log("Utente modificato:");
+        //console.log(result);
+        res.status(200).json({success: true, message: "Account modificato"});
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ success: false, message: "Errore non gestito" });
+    }
+})
+
+app.delete('/user/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+        const client = await MongoClient.connect(mongoURL);
+        const coll = client.db('Fastfood').collection('user');
+        const result = await coll.deleteOne({_id: new ObjectID(id)});
+
+        console.log("Utente cancellato:");
+        console.log(result);
+        res.status(200).json({success: true, message: "Account cancellato"});
+        await client.close();
+      
+    } catch (error) {
+        res.status(500).json({ success: false, message: "Errore non gestito" });
+    }
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
